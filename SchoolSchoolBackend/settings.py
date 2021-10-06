e91f45979aa79957a14c17ac8931d7052136b39f
xphil-22 <75225050+xphil-22@users.noreply.github.com>
2021-10-06 22:49:43 +0200

    a

--- SchoolSchoolBackend/settings.py
+++ SchoolSchoolBackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m@jgmdq^iv8weub-stvagg)j!wmbyw2gxmz110a=oq^y3ph83j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1','']#
 
 
 # Application definition
