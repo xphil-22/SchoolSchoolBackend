92a6ba2f6a049f65468b8fb9d15283f6a7480ff5
xphil-22 <75225050+xphil-22@users.noreply.github.com>
2021-10-11 19:22:19 +0200

    a

--- SchoolSchoolBackend/settings.py
+++ SchoolSchoolBackend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m@jgmdq^iv8weub-stvagg)j!wmbyw2gxmz110a=oq^y3ph83j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','schoolschooli.herokuapp.com']
 
