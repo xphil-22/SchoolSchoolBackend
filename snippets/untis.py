dafd89fbf09d9fc5336d2e0a9ac23068cb03fb74
xphil-22 <75225050+xphil-22@users.noreply.github.com>
2021-11-12 12:06:19 +0100

    modified untis

--- snippets/untis.py
+++ snippets/untis.py
@@ -1,11 +1,12 @@
 import webuntis
 import datetime
 import json
+import ast
 
 class Untis:
     def __init__(self):
         self._session = 0
-    
+        
     def newSession(self, username, password):
         try:
             s = webuntis.Session(
@@ -16,10 +17,36 @@ class Untis:
                 useragent='WebUntis Test'
             ).login()
             self._session = s
-
+            
             return True
 
         except:
-            print("False")
             return False
+    
+    def getClasses(self):
+        return self._session.klassen()
+    
+    def getSubjects(self, className):
+        today = datetime.date.today()
+        monday = today - datetime.timedelta(days=today.weekday())
+        friday = monday + datetime.timedelta(days=6)
+
+        klasse = self._session.klassen().filter(name=className)[0]  
+        tt = self._session.timetable(klasse=klasse, start=monday, end=friday)
+        
+        ID_List = []
+        for subject in tt:       
+            s = str(subject)
+            ID_List.append(int(s[s.find('su')+13:s.find(']', s.find('su'))-1]))
+
+        ID_List = list(set(ID_List)) #To clear the array from duplicates
+            
+        subject_list = [subject for subject in self._session.subjects()
+                        if subject.id in ID_List]
+        
+        subject_list_pretty = []
+        for sub in subject_list:
+            subject_list_pretty.append(str(sub)+" ")
+        
+        return subject_list_pretty
     
