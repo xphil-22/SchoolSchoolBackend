e848985f1d0f383153b94e8d7bd2fbaaf1bbd689
xphil-22 <75225050+xphil-22@users.noreply.github.com>
2021-11-20 18:53:55 +0100

    Update untis.py

--- snippets/untis.py
+++ snippets/untis.py
@@ -113,6 +113,9 @@ class WebsiteUntis:
         tt = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div/div/div[2]/div[1]/div/a[5]/div/div/div[2]')))
         tt.click()
         
+        elements = driver.find_elements(By.TAG_NAME, 'button')
+        for i in elements:
+            print(i+"\n")
         
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'embedded-webuntis')))
         driver.switch_to.frame('embedded-webuntis')
