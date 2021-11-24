6663876ba007432962e364679cfda62b27077faf
xphil-22 <75225050+xphil-22@users.noreply.github.com>
2021-11-24 18:35:28 +0100

    Update untis.py

--- snippets/untis.py
+++ snippets/untis.py
@@ -122,7 +122,7 @@ class WebsiteUntis:
 
        
         try:
-            ical_Download_Button = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//*[@id='dijit_layout__LayoutWidget_0']/section/div/div")))
+            ical_Download_Button = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//*[@id='dijit_layout__LayoutWidget_0']/section/header")))
             return str(ical_Download_Button)
         except:       
             return "False"
