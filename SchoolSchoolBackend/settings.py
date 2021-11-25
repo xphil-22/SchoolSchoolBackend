49bca64caa035278ebe0d9414f191249a32b2cae
xphil-22 <75225050+xphil-22@users.noreply.github.com>
2021-11-25 15:17:11 +0100

    Update settings.py

--- SchoolSchoolBackend/settings.py
+++ SchoolSchoolBackend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-m@jgmdq^iv8weub-stvagg)j!wmbyw2gxmz110a=oq^y3ph83j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','schoolschooli.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','schoolschooli.herokuapp.com','schoolschoolitest.herokuapp.com]
 
 
 # Application definition
