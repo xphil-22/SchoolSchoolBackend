a1d7fc62d84fe7687006f304a7e4834f332c005b
xphil-22 <75225050+xphil-22@users.noreply.github.com>
2021-11-30 17:33:18 +0100

    a

--- snippets/untis.py
+++ snippets/untis.py
@@ -84,7 +84,7 @@ class WebsiteUntis:
         self._options = webdriver.ChromeOptions()
         self._options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
         self._options.add_argument("--no-sandbox")
-        #self._options.add_argument("--headless")
+        self._options.add_argument("--headless")
         self._options.add_argument("--disable-dev-sh-usage")
         prefs = {"profile.default_content_settings.popups": 0,
              "download.default_directory": 
@@ -135,8 +135,8 @@ class WebsiteUntis:
             
     def downloadIcal(self, Data, Processes):
         UserData = f"{self._username}{self._password}"
-        driver = webdriver.Chrome(chrome_options=self._options)
-        #driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=self._options)
+        #driver = webdriver.Chrome(chrome_options=self._options)
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=self._options)
         driver.set_window_position(0, 0)
         driver.set_window_size(1902, 768)
         
@@ -146,14 +146,14 @@ class WebsiteUntis:
         input_fields[1].send_keys(self._password)
         driver.find_element(By.CLASS_NAME, 'redesigned-button').click()
 
-        tt = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div/div/div[2]/div[1]/div/a[5]/div/div/div[2]')))
+        tt = WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div/div/div[2]/div[1]/div/a[5]/div/div/div[2]')))
         tt.click()
         driver.refresh()
-        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, 'embedded-webuntis')))
+        WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.ID, 'embedded-webuntis')))
         driver.switch_to.frame('embedded-webuntis')
 
         sel = '#dijit_layout__LayoutWidget_0 > section > div > div > div.un-flex-pane.un-flex-pane--fixed.un-timetable-page__header > div > form > div.float-right.btn-group > button:nth-child(1)'
-        ical_Download_Button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, sel)))
+        ical_Download_Button = WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.CSS_SELECTOR, sel)))
         
         ical_Download_Button.click()
         while os.path.exists(self._filePath) == False:
