18180b6d1f23554e73f4fb96f83680b62415f5df
xphil-22 <75225050+xphil-22@users.noreply.github.com>
2021-11-30 17:28:23 +0100

    a

--- snippets/untis.py
+++ snippets/untis.py
@@ -16,9 +16,9 @@ from selenium.webdriver.chrome.options import Options
 from snippets import Ical
 import os
 
-from multiprocessing import Process
-from snippets import Async
+from threading import Thread
 class Untis:
+    
     def __init__(self):
         self._session = 0
         
@@ -71,55 +71,60 @@ class Untis:
 
 class WebsiteUntis:
     
+    Data = []
+    Threads = []
+    
     def __init__(self, username, password):
+        
         self._ical = 0
         self._username = username
         self._password = password
         self._filePath = ""
-        self._Async = Async.Async()
         
         self._options = webdriver.ChromeOptions()
         self._options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
         self._options.add_argument("--no-sandbox")
-        self._options.add_argument("--headless")
+        #self._options.add_argument("--headless")
         self._options.add_argument("--disable-dev-sh-usage")
         prefs = {"profile.default_content_settings.popups": 0,
              "download.default_directory": 
                         os.getcwd() + "\Ical_Files", #Current Directory
              "directory_upgrade": True}
         self._options.add_experimental_option("prefs", prefs)
-        
+    
+
     def getWebSubjects(self):
-        if self.proofAsyncData() == False and [self._username, self._password] not in self._Async.getData(): 
-            self._setFilePath()
-            self._Async.addProcess(f"{self._username}{self._password}")
-            
-            self.downloadIcal()
-            p1 = Process(target=self.downloadIcal)
-            p1.start()
-            return "Collecting startet, please wait..."
-        
-        elif f"{self._username}{self._password}" in self._Async.getProcesses():
-            return "Collecting data, please wait..."
-        
-        else:    
-            return self._Async.getData[f"{self._username}{self._password}"]
+                UserData = f"{self._username}{self._password}"
+                print(UserData)
+                print(str(WebsiteUntis.Threads) + str(WebsiteUntis.Data))
+                
+                if UserData not in WebsiteUntis.Threads and not self.proofData():
+                    self._setFilePath()
+                    WebsiteUntis.Threads.append(UserData)
+                    t = Thread(target=self.downloadIcal, args=(WebsiteUntis.Data, WebsiteUntis.Threads))
+                    t.start()
+                    return "Collecting startet, please wait..."
+                
+                if UserData in WebsiteUntis.Threads and not self.proofData():
+                    return "Collecting data, please wait..."
+                
+                if self.proofData():
+                    data = [el[UserData] for el in WebsiteUntis.Data if UserData in el]
+                    WebsiteUntis.Data = [el for el in WebsiteUntis.Data if UserData not in el]
+                    return data
+
+                else:
+                    return "Fehler ..."
+                
+    def proofData(self):
+        UserData = f"{self._username}{self._password}"
+        for el in WebsiteUntis.Data:
+            if UserData in el:
+                return True
+        return False
     
-    def proofAsyncData(self):
-        for el in self._Async.getData():
-            if f"{self._username}{self._password}" in el:
-                return el[f"{self._username}{self._password}"]
-        else:
-            return False
-    
-    def runInParallel(*fns):
-        proc = []
-        for fn in fns:
-            p = Process(target=fn)
-            p.start()
-            proc.append(p)
-        for p in proc:
-            p.join()
+    def proofTime(self):
+        pass
     
     def _setFilePath(self):
         name = self._username.replace('ss','ß').split('.')
@@ -128,8 +133,8 @@ class WebsiteUntis:
         if os.path.exists(self._filePath):
             os.remove(self._filePath)
             
-    def downloadIcal(self):
-        print("download Ical:" , self._Async.getData())
+    def downloadIcal(self, Data, Processes):
+        UserData = f"{self._username}{self._password}"
         driver = webdriver.Chrome(chrome_options=self._options)
         #driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=self._options)
         driver.set_window_position(0, 0)
@@ -141,15 +146,14 @@ class WebsiteUntis:
         input_fields[1].send_keys(self._password)
         driver.find_element(By.CLASS_NAME, 'redesigned-button').click()
 
-        tt = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div/div/div[2]/div[1]/div/a[5]/div/div/div[2]')))
+        tt = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div/div/div[2]/div[1]/div/a[5]/div/div/div[2]')))
         tt.click()
-        
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'embedded-webuntis')))
+        driver.refresh()
+        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, 'embedded-webuntis')))
         driver.switch_to.frame('embedded-webuntis')
 
-       
         sel = '#dijit_layout__LayoutWidget_0 > section > div > div > div.un-flex-pane.un-flex-pane--fixed.un-timetable-page__header > div > form > div.float-right.btn-group > button:nth-child(1)'
-        ical_Download_Button = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, sel)))
+        ical_Download_Button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, sel)))
         
         ical_Download_Button.click()
         while os.path.exists(self._filePath) == False:
@@ -157,9 +161,7 @@ class WebsiteUntis:
             
         self._ical = Ical.Ical(self._filePath)
         data = self._ical.getSubjectData()
-        self._Async.addData({f"{self._username}{self._password}" : data})
-        print(data)
-        self._Async.removeProcess(f"{self._username}{self._password}")
-
+        Data.append({UserData : data})
+        Processes.remove(f"{self._username}{self._password}")
 
 
