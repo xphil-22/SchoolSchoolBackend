2d5b6bbe0d6fed5ea8e31fe88a09e2de0ae326ad
xphil-22 <75225050+xphil-22@users.noreply.github.com>
2021-12-02 20:53:29 +0100

    ö

--- snippets/Ical.py
+++ snippets/Ical.py
@@ -1,13 +1,26 @@
 from django.core.files import File
-
+import os
 class Ical:
-   
+    
     def __init__(self, FilePath):
-        self._IcalString = ""
-        with open(FilePath, 'r') as f:
-            myFile = File(f)
+        print(FilePath)
+        self._filePaths = []
+        self._IcalStrings = []
+        
+        if os.path.exists(FilePath):
+            self._filePaths.append(FilePath)
             
-            self._IcalString = myFile.read()
+        if os.path.exists(f"{FilePath[:-4]} (1).ics"):
+            self._filePaths.append(f"{FilePath[:-4]} (1).ics")
+            
+        if os.path.exists(f"{FilePath[:-4]} (2).ics"):
+            self._filePaths.append(f"{FilePath[:-4]} (2).ics")
+        
+        for i in range(len(self._filePaths)):
+            print(i)
+            with open(self._filePaths[i], 'r') as f:
+                myFile = File(f)
+                self._IcalStrings.append(myFile.read())
            
     def getSpecificData(self, key):
         komma = self._IcalString.find(':' , self._IcalString.find(key))+1
@@ -15,14 +28,18 @@ class Ical:
 
     def getSubjectData(self):
         SubjectData = []
-        arr = self._IcalString.split('\n')
-        for i in range(len(arr)):
-            if "LOCATION" in arr[i]:
-                a = arr[i][arr[i].find(':')+1:]
-                b = a.split(' ')
-                c = b[-2]
-                element = [arr[i+1][arr[i+1].find(':')+1:] , c] 
-                if element not in SubjectData:
-                    SubjectData.append(element)               
+        for i in range(len(self._filePaths)):
+            arr = self._IcalStrings[i].split('\n')
+            for i in range(len(arr)):
+                if "LOCATION" in arr[i]:
+                    a = arr[i][arr[i].find(':')+1:]
+                    b = a.split(' ')
+                    c = b[-2]
+                    element = [arr[i+1][arr[i+1].find(':')+1:] , c] 
+                    if element not in SubjectData:
+                        SubjectData.append(element)     
         return SubjectData
     
+if __name__ == '__main__':
+    i = Ical("Ical_Files\KuhlmaMay.ics")
+    print(i.getSubjectData())
