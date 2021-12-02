a671d05d25326d42c838e80a9e93c7839aefca63
xphil-22 <75225050+xphil-22@users.noreply.github.com>
2021-12-02 21:00:01 +0100

    a

--- snippets/untis.py
+++ snippets/untis.py
@@ -159,7 +159,7 @@ class WebsiteUntis:
     def startSelenium(self):
         driver = webdriver.Chrome(chrome_options=self._options)
         if not local:
-            driver = driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=self._options)
+            driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=self._options)
             
         print("1")
         driver.set_window_position(0, 0)
