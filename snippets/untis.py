dc22537f522869c5f05e45b605179aa5f8ff9209
xphil-22 <75225050+xphil-22@users.noreply.github.com>
2021-12-02 21:16:54 +0100

    a

--- snippets/untis.py
+++ snippets/untis.py
@@ -85,7 +85,9 @@ class WebsiteUntis:
         self._options = webdriver.ChromeOptions()
         self._options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
         self._options.add_argument("--no-sandbox")
-        #self._options.add_argument("--headless")
+        if not local:
+            self._options.add_argument("--headless")
+        #self._options.add_argument("--headless")            
         self._options.add_argument("--disable-dev-sh-usage")
         
         path =  "tmp/" #Current Directory
